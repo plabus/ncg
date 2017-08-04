9f3a9e3abc0509e00b628e2c0f4291ac45e766f2
Peter Labus <plabus@sissa.it>
2017-08-04 13:43:09 +0200

    Added independent definition of NUM_M and WRITOUT_FREQ changed to be measured in sweeps.

--- Constants.h
+++ Constants.h
@@ -6,21 +6,21 @@
 #define REAL double
 
 // Fundamental Parameters
-#define N 5
+#define N 25
 #define P 1
-#define Q 3
-#define G2 0.f
-#define G4 1.f
+#define Q 0
+#define G2 (1.0)
+#define G4 (0.0)
 
 // Derived Quantities
 #define D (P+Q)
 #define S ((Q-P+8)%8)
 #define K ( (D%2)?(int)pow(2,(D-1)/2):(int)pow(2,D/2) )
 #define SWEEP (N*N)
-#define NUM_M (NUM_H+NUM_L)
+#define NUM_M ((int)pow(2,(D-1)))
 
 // User Parameters
 #define CHAIN_LENGTH (20*SWEEP)
-#define WRITEOUT_FREQ (2*SWEEP)
+#define WRITEOUT_FREQ 2 // in SWEEPs
 
 #endif
